3b25a29c584c7951cb7a78b09ad40fd03a3eedfc
Maycon <maycon5591@gmail.com>
2024-02-03 23:14:58 -0300

    Ajustada alteracao de tarefas entre os quadros

--- src/app/tarefas-view/tarefas-view.component.ts
+++ src/app/tarefas-view/tarefas-view.component.ts
@@ -5,37 +5,63 @@ import {
   CdkDrag,
   CdkDropList,
 } from '@angular/cdk/drag-drop';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { TarefaService } from '../services/tarefa-service.service';
+import { Tarefa } from '../model/tarefa.model';
+import { SituacaoTarefaEnum } from '../shared/enums/situacaoTarefa.enum';
 
 @Component({
   selector: 'app-tarefas-view',
   templateUrl: './tarefas-view.component.html',
   styleUrls: ['./tarefas-view.component.css']
 })
-export class TarefasViewComponent {
+export class TarefasViewComponent implements OnInit, OnDestroy {
 
-  lista1 = [
-    {name: 'teste1'},
-    {name: 'teste2'},
-  ]
+  pendente: Tarefa[] = [];
 
-  lista2 = [
-    {name: 'teste1'},
-    {name: 'teste2'},
-  ]
+  fazendo: Tarefa[] = [];
 
-  lista3 = [
-    {name: 'teste1'},
-    {name: 'teste2'},
-  ]
+  analise: Tarefa[] = [];
 
-  lista4 = [
-    {name: 'teste1'},
-    {name: 'teste2'},
-  ]
+  concluida: Tarefa[] = [];
+
+  constructor(private tarefaSerice: TarefaService) { }
+
+  ngOnInit(): void {
+    this.tarefaSerice.getTarefas().subscribe(data => {
+      this.filtrarTarefas(data);
+    });
+  }
+  ngOnDestroy(): void {
+  }
 
 
-  drop(event: CdkDragDrop<{ name: string; }[]>) {
+  drop(event: CdkDragDrop<Tarefa[]>) {
+    const previousContainerIdx = event.previousContainer.id.replace(/\D/g, '');
+    const newSituacao = parseInt(event.container.id.replace(/\D/g, ''));
+    let tarefa: Tarefa;
+
+    switch (previousContainerIdx) {
+      case "0":
+        this.pendente[event.currentIndex].situacao = newSituacao;
+        tarefa = this.pendente[event.currentIndex];
+        break;
+      case "1":
+        this.fazendo[event.currentIndex].situacao = newSituacao;
+        tarefa = this.fazendo[event.currentIndex];
+        break;
+      case "2":
+        this.analise[event.currentIndex].situacao = newSituacao;
+        tarefa = this.analise[event.currentIndex];
+        break;
+      default:
+        this.concluida[event.currentIndex].situacao = newSituacao;
+        tarefa = this.concluida[event.currentIndex];
+        break;
+    }
+
+    this.tarefaSerice.update(tarefa);
+
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
@@ -46,6 +72,19 @@ export class TarefasViewComponent {
         event.currentIndex,
       );
     }
+
+    console.log(this.fazendo)
+  }
+
+  filtrarTarefas(data: Tarefa[]) {
+    this.pendente = data.filter(t => t.situacao == SituacaoTarefaEnum.Pendente);
+    this.fazendo = data.filter(t => t.situacao == SituacaoTarefaEnum.Fazendo);
+    this.analise = data.filter(t => t.situacao == SituacaoTarefaEnum.Analise);
+    this.concluida = data.filter(t => t.situacao == SituacaoTarefaEnum.Concluida);
+  }
+
+  setTarefaSituacao() {
+
   }
 
 }
