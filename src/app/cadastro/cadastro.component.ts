e9a00b151fbf51351ab2951409155d13e9428471
Maycon <maycon5591@gmail.com>
2024-04-18 23:48:53 -0300

    Ajustes na tela de cadastro

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { AuthService } from '../auth/auth.service';
 import { User } from '../model/user.model';
+import { MensagemService } from '../services/mensagem.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -11,20 +12,27 @@ import { User } from '../model/user.model';
 export class CadastroComponent implements OnInit, OnDestroy {
   form: FormGroup;
 
-  constructor(private authService: AuthService){}
+  constructor(private authService: AuthService,
+              private messageService: MensagemService
+  ){}
 
 
   ngOnInit(): void {
     this.form = new FormGroup({
       name: new FormControl(null),
+      sobrenome: new FormControl(null),
+      telefone: new FormControl(null),
       email: new FormControl("asdasdda@hotmail.com"),
-      tipo: new FormControl(null),
-      password: new FormControl(null)
+      password: new FormControl(null),
+      confirm: new FormControl(null)
     });
   }
 
   onSubmit(){
-    const value = { ...this.form.value, tipo: +this.form.value.tipo };
+    if(this.form.get('password')?.value != this.form.get('confirm')?.value) {
+      return this.messageService.ShowMessage("As senhas devem ser iguais", 3000, false);
+    }
+    const value = this.form.value;
     this.authService.cadastro(value).subscribe(data => {
     });
   }
